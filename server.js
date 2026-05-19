56cf1976c849cb92701842e0508b38f400906ca3
Carl Ivan Ken Simbulan <carlivan252003@gmail.com>
2026-05-20 00:25:47 +0800

    S

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('CarGO Backend is running!'));
-
 app.get('/api/health', async (req, res) => {
   const checks = {
     mongoUri: !!process.env.MONGO_URI,
